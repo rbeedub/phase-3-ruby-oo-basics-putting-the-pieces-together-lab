4363a498a97bf910df94ae572f598ec600d438d2
Rebecca Wollard <rbwollard@gmail.com>
2023-01-31 15:30:59 -0700

    commit tree 84485cd7d4f444aed442bbadee6cd7a5a6f2a358

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+# Make your shoe class here!require 'pry'
+require 'pry'
+
+class Shoe
+ 
+    attr_reader :brand
+    attr_accessor :color, :size, :condition, :material
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble 
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+    end 
+end
+
+binding.pry
+0
